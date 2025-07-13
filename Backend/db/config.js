85e22aff7c2f64475d6d9d1fa61d77138ca89a1d
VishankhBhardwaj <pushkar.vikas8@yahoo.com>
2025-07-13 22:38:35 +0530

    small change

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://pushkarvikas7:AzTdD77M7WlRjYJl@cluster0.0xzfcjj.mongodb.net/LuxuryEstate')
-.then(() => console.log('Connected to MongoDB...')).catch(err => console.error('Could not connect to MongoDB...', err));
+const username = process.env.USERNAME;
+const password = process.env.DB_PASSWORD;
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.0xzfcjj.mongodb.net/LuxuryEstate`;
+
+mongoose.connect(uri)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
