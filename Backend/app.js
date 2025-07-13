290074599d266c911a37edbc30a0fcd3454edf53
VishankhBhardwaj <pushkar.vikas8@yahoo.com>
2025-07-13 23:22:46 +0530

    bug fix

--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 require('./db/config');
 require("dotenv").config();
+const app = express();
 
+
+// Middleware
 app.use(cors({
   origin: ["https://luxury-estate-lemon.vercel.app"],
   credentials: true
 }));
-
-// Middleware
-app.use(cors());
 app.use(express.json({ limit: "100mb" })); 
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
